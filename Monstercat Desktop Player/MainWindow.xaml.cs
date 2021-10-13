b5c8046da1ad57f43931d6af966048db654eac49
Adam Silva-Nitz <tsilva1919@gmail.com>
2021-10-13 01:41:15 -0600

    Reworking project with WPF, fixed issue where website wouldn't load

---
+++ Monstercat Desktop Player/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace Monstercat_Desktop_Player
+{
+    public partial class MainWindow : Window
+    {
+        public ChromiumWebBrowser browser;
+
+        public void InitBrowser()
+        {
+            CefSettings settings = new CefSettings();
+            settings.CachePath = Environment.CurrentDirectory + @"\CEF";
+
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser("https://www.monstercat.com/player");
+            Grid.SetRow(browser, 0);
+            MainGrid.Children.Add(browser);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+    }
+}
